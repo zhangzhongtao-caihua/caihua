5552291efd7a96c83427e30a2b4f07d0e6819f12
Caihua <347091704@qq.com>
2021-05-19 22:17:35 +0800

    测试提交2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("tao","qqq")
+        Log.d("tao","wwwww")
     }
 }
